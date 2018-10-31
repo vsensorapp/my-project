1670715e67affed3d7a73d4ba5b3afc143159066
vsensorapp <43778115+vsensorapp@users.noreply.github.com>
2018-10-31 14:36:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,28 +4,28 @@ importScripts('/_nuxt/workbox.5c678697.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/59fcc3ef66019eef8e35.js",
-    "revision": "d1316b2ddf26a31d3878136b164f2669"
+    "url": "/_nuxt/08a0efd14577a49747f5.js",
+    "revision": "066728e98e9b17a4bd405729d4ae738c"
   },
   {
-    "url": "/_nuxt/76a3d897d5928842d78e.js",
-    "revision": "3a57bd9c4be4dc725182d5baa19deaf9"
+    "url": "/_nuxt/1e90780e4e7a7b7742c3.js",
+    "revision": "26fe7b8aff488888959bf90d7c92946c"
   },
   {
-    "url": "/_nuxt/89e22006c15ed7ba911f.js",
-    "revision": "37db860ede112f7f780cabc09f2303df"
+    "url": "/_nuxt/462fc972c391ae687163.js",
+    "revision": "61028ba56449d3881354cc2d8f4508de"
   },
   {
-    "url": "/_nuxt/b26e33e6bf4dc72ef494.js",
-    "revision": "4f13ea5dcd318912e6644686e2f1dd55"
+    "url": "/_nuxt/b415957394147a3a8c9d.js",
+    "revision": "b3b437c59f68d8ee20a4e07892b3ce9b"
   },
   {
-    "url": "/_nuxt/cce7833aad1b045387a1.js",
-    "revision": "9e2231d6e7ec726eaf460ad3fd65c780"
+    "url": "/_nuxt/c6526e647d83c8df2a21.js",
+    "revision": "a1173c2a51b189e53858b492d7f8e370"
   },
   {
-    "url": "/_nuxt/fb77af55c03f56493e1b.js",
-    "revision": "e14580c60db81c33fe25e653f19ca744"
+    "url": "/_nuxt/e8e1c5b6b9853bddcd22.js",
+    "revision": "da21b03853ed1156225e3812626fa15c"
   }
 ], {
   "cacheId": "V-Sensor",
