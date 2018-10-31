bb759b21ae67e77da9ceaf143e70f75d0d78e675
vsensorapp <43778115+vsensorapp@users.noreply.github.com>
2018-10-31 14:55:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,20 +4,20 @@ importScripts('/_nuxt/workbox.5c678697.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/_nuxt/207dffb70d6b4adcf208.js",
+    "revision": "9d68ceedc92d95bb1de1e2cbbdad70b6"
+  },
+  {
     "url": "/_nuxt/23af313969d4e9a0e28b.js",
     "revision": "20647eb9f94433b9bbebd26876db2170"
   },
   {
-    "url": "/_nuxt/38832d499861a1d406b6.js",
-    "revision": "af411f0b60ebf08120266444a235deb3"
+    "url": "/_nuxt/9eef0b8b09412ea666e5.js",
+    "revision": "946f66f240628f1bc482185082832265"
   },
   {
     "url": "/_nuxt/a136249191e35830b163.js",
     "revision": "f5b78344856dc2160ab419ad194190ad"
-  },
-  {
-    "url": "/_nuxt/dfde04a13557886e2d55.js",
-    "revision": "32d6cb1575c15ef6ae708b50fab13bac"
   }
 ], {
   "cacheId": "V-Sensor",
